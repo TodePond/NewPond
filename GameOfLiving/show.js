446060cd5e5a4444bbfb43dfcad96d468e5f3ac1
Luke Wilson <l2wilson94@gmail.com>
2022-03-07 17:52:47 +0000

    fix show not starting

--- GameOfLiving/show.js
+++ GameOfLiving/show.js
@@ -2,11 +2,9 @@ const Show = {}
 
 {
 
-	
-
 	Show.start = ({canvas, context, paused = false, scale = 1.0, speed = 1.0, resize = () => {}, tick = () => {}} = {}) => {
 		
-		const show = {canvas, context, paused, scale, speed, resize, tick, construct}
+		const show = {canvas, context, paused, scale, speed, resize, tick}
 
 		if (document.body === null) {
 			addEventListener("load", () => start(show))
